fcae36dad76305307641430abc9b8ea23818ead0
micahblouin <63933879+micahblouin@users.noreply.github.com>
2021-02-02 15:22:13 -0600

    Update main.py
    prompt user for input and Use qty 4 of ingredient

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+name = input('Please enter the Ingredient Name: ')
+purchPrice = input('Please enter the most recent Purchase Price: ')
+purchUnit = input('Please enter the most recent Purchase Unit: ')
+useUnit = input('Please enter the Usage Unit: ')
+wstPrcnt = input('Please enter the Waste Percentage: ')
+
+
 class Ingredient:
     def __init__(self, name, purchPrice, purchUnit, useUnit, wstPrcnt):
         self.name = name
@@ -9,7 +16,7 @@ class Ingredient:
 # calculates cost as purchased per use
 
     def useRaw(self, use):
-        return float(self.purchPrice) * float(use)
+        return round(float(self.purchPrice) * float(use),2)
 
 
 # calculates cost as purchased per use
@@ -18,11 +25,16 @@ class Ingredient:
         return float(use) / (1 - float(self.wstPrcnt) / 100) * float(
             self.purchPrice)
 
-sugar = Ingredient('sugar', 0.65, 'lb', 'cup', 0)
-apple = Ingredient('apple', 1.18, 'lb', 'lb', 30)
+Ingredient1 = Ingredient(name, purchPrice, purchUnit, useUnit, wstPrcnt)
 
-print(apple.useRaw(8))
-print(round(apple.useYield(8), 2))
+print(f'$ {Ingredient1.useRaw(4)}')
+
+#sugar = Ingredient('sugar', 0.65, 'lb', 'cup', 0)
+#apple = Ingredient('apple', 1.18, 'lb', 'lb', 30)
+
+#print(apple.useRaw(8))
+#print(round(apple.useYield(8), 2))
+
 """
 import gc
 
