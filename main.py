e72720c033c01e20776d06eb42a201914b6e3d4f
micahblouin <63933879+micahblouin@users.noreply.github.com>
2021-02-03 09:40:18 -0600

    Update main.py
    cleaned code.
    Added pprint functionality
    Instantiates Ingredient & Returns all vars

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from pprint import pprint
+
 name = input('Please enter the Ingredient Name: ')
 purchPrice = input('Please enter the most recent Purchase Price: ')
 purchUnit = input('Please enter the most recent Purchase Unit: ')
 useUnit = input('Please enter the Usage Unit: ')
 wstPrcnt = input('Please enter the Waste Percentage: ')
-
 
 class Ingredient:
     def __init__(self, name, purchPrice, purchUnit, useUnit, wstPrcnt):
@@ -18,7 +19,6 @@ class Ingredient:
     def useRaw(self, use):
         return round(float(self.purchPrice) * float(use),2)
 
-
 # calculates cost as purchased per use
 
     def useYield(self, use):
@@ -27,19 +27,5 @@ class Ingredient:
 
 Ingredient1 = Ingredient(name, purchPrice, purchUnit, useUnit, wstPrcnt)
 
-print(f'$ {Ingredient1.useRaw(4)}')
-
-#sugar = Ingredient('sugar', 0.65, 'lb', 'cup', 0)
-#apple = Ingredient('apple', 1.18, 'lb', 'lb', 30)
-
-#print(apple.useRaw(8))
-#print(round(apple.useYield(8), 2))
-
-"""
-import gc
-
-for obj in gc.get_objects():
-    if isinstance(obj, Ingredient):
-        print(obj.name)
-
-"""
+print(f'New Ingredient Added! \n')
+pprint(vars(Ingredient1))
